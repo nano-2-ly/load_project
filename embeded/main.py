790a3c31e2398584e33fa86df8320f387cb8ad3f
nano-2-ly <dong991207@unist.ac.kr>
2019-07-05 10:33:48 +0900

    trivial error fix

--- embeded/main.py
+++ embeded/main.py
@@ -7,13 +7,15 @@ import json
 # declare server and packet_reactor
 sv = server()
 pr = packet_reactor()
-
-# receive data from server
 sv.socket_open()
-received_data = sv.recv_data()
-if received_data['description'] == "Status":
-    Status_data = pr.received_data
-    sv.send_data(json.dumps(Status_data))
-else:
-    pr.packet_transmit(received_data['description'], received_data['data'])
+while 1:
+	# receive data from server
+	print('re')
+	received_data = sv.recv_data()
+	print(received_data)
+	if received_data['description'] == "Status":
+		Status_data = pr.received_data
+		sv.send_data(json.dumps(Status_data))
+	else:
+		pr.packet_transmit(received_data['description'], received_data['data'])
 
