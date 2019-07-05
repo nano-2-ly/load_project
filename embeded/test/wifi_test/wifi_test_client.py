321b18e9c0c886734da37109576973f80f75d5b3
nano-2-ly <dong991207@unist.ac.kr>
2019-07-05 10:35:36 +0900

    client example fix

--- embeded/test/wifi_test/wifi_test_client.py
+++ embeded/test/wifi_test/wifi_test_client.py
@@ -1,7 +1,8 @@
 import json
 from socket import *
 
-server.socket(AF_INET, SOCK_STREAM)
+server = socket(AF_INET, SOCK_STREAM)
 
-server.connect(('192.168.0.68'))
-sever.sendall(json.dumps({'description':'LED control', 'data':'1111111111'}))
+server.connect(('192.168.0.68',50000))
+server.sendall(json.dumps({'description':'LED control', 'data':'0000000000'}).encode())
+
