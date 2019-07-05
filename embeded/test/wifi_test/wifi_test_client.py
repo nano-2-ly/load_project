45612584e3528f43953c29a705801b4ced958927
nano-2-ly <dong991207@unist.ac.kr>
2019-07-05 11:54:13 +0900

    client test update

--- embeded/test/wifi_test/wifi_test_client.py
+++ embeded/test/wifi_test/wifi_test_client.py
@@ -3,6 +3,17 @@ from socket import *
 
 server = socket(AF_INET, SOCK_STREAM)
 
-server.connect(('192.168.0.68',50000))
-server.sendall(json.dumps({'description':'LED control', 'data':'0000000000'}).encode())
 
+server.connect(('192.168.0.68',50000))
+server.sendall(json.dumps({'description':'BLDC motor control', 'data': {'Break':'Break disable', 'Direction' : 'CW', 'Speed' : 1000}}).encode())
+
+'''
+server.sendall(json.dumps({'description':'Status'}).encode())
+
+json_data = server.recv(1024).decode('utf-8')
+print(json_data)
+msg = json.loads(json_data)
+print(msg)
+print(type(msg))
+
+'''
